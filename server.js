07219c0af409e7b1c55081a3e771c64c14156ea8
mirahazall <s223530601@deakin.edu.au>
2023-12-06 10:41:42 +1100

    first commit

--- server.js
+++ server.js
@@ -1,21 +1,66 @@
+let express = require("express");
+let app = express();
 
 
-const express = require('express');
-const app = express();
-const projectsRoute = require('./routes/projects');
+//dbConnect.dbConnect()
+//var app = require('express')();
+let http = require('http').createServer(app);
+let io = require('socket.io')(http);
+//const MongoClient = require('mongodb').MongoClient;
 
-app.use(express.static(__dirname + '/public'));
-// Middleware to parse JSON and URL-encoded data
+
+
+
+
+// routes
+let projectsRoute = require('./routes/projects')
+
+
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.static(__dirname + '/public'));
+app.use('/api/projects',projectsRoute)
 
-// Using the projectsRoute for the /api/projects path
-app.use('/api/projects', projectsRoute);
+
+app.get("/test", function (request, response) {
+  var user_name = request.query.user_name;
+  response.end("Hello " + user_name + "!");
+});
+
+app.get('/addTwoNumbers/:firstNumber/:secondNumber', function(req,res,next){
+  var firstNumber = parseInt(req.params.firstNumber) 
+  var secondNumber = parseInt(req.params.secondNumber)
+  var result = firstNumber + secondNumber || null
+  if(result == null) {
+    res.json({result: result, statusCode: 400}).status(400)
+  }
+  else { res.json({result: result, statusCode: 200}).status(200) } 
+})
+
+//socket test
+io.on('connection', (socket) => {
+  console.log('a user connected', socket.id);
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+  setInterval(()=>{
+    socket.emit('number', parseInt(Math.random()*10));
+  }, 1000);
+
+});
+
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log('App listening to port: ' + port);
 });
+
+
+
+
+
 
 
 
